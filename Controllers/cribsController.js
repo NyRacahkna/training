ccaad13c99eb0daeb302d63cfef5409e2e0052d0
rachana <ny_rachana@mef.gov.kh>
2018-08-02 10:23:23 +0700

    first init

---
+++ Controllers/cribsController.js
@@ -0,0 +1,23 @@
+angular
+    .module('ngCribs')
+    .controller('cribsController', function($scope){
+        $scope.students = [
+            {
+                "type" : "Pheakdey",
+                "gender" : "Female",
+                "address" : "Phnom Penh",
+                "academic" : "Year 4"
+            }, {
+                "type" : "Pheakdey",
+                "gender" : "Female",
+                "address" : "Phnom Penh",
+                "academic" : "Year 4"
+            }, {
+                "type" : "Pheakdey",
+                "gender" : "Female",
+                "address" : "Phnom Penh",
+                "academic" : "Year 4"
+            },
+            
+        ];
+    });
